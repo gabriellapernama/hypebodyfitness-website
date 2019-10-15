6c48dc6c943a796a23983ec4d12cf425a3c1e49c
Gabriella Pernama <gabypernama@gmail.com>
2019-10-15 15:46:44 -0700

    created basis for index home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,30 @@
 import React from "react"
 import NavBar from '../components/navigationBar.js';
+import Welcome from '../components/welcome.js';
+import AboutMe from '../components/aboutMe.js';
+import Testimonials from '../components/testimonials.js';
+import ServicesOffered from '../components/servicesOffered.js';
+import Contact from '../components/contact.js';
 
 export default () => {
+  if (typeof window !== "undefined") {
+    require("smooth-scroll")('a[href*="#"]', {
+      speed: 1000,
+      speedAsDuration: true,
+      easing: 'easeInOutCubic'
+    })
+  }
+
 
 return <div>
     <div><NavBar /></div>
-    Welcome to HypeBodyFitness! 
-    <div></div>
+    <div>
+        <Welcome />
+        <AboutMe id="aboutMe" />
+        <Testimonials id="testimonials" />
+        <ServicesOffered id="servicesOffered" />
+        <Contact id="contact" />
+    </div>
     </div>
 
 }
